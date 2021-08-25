7f1affb6d86d1b8c36b653bdf4c44eed6ccf4b1e
SumitKuSahu <1929252@kiit.ac.in>
2021-08-25 20:32:02 +0530

    added splash screen and home screen files

---
+++ lib/Screens/splash_screen.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'homeScreen.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  static const String id="splash-screen";
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(Duration(
+      seconds: 3,
+    ), () {
+      Navigator.pushReplacementNamed(context, HomeScreen.id);
+    });
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Hero(
+            tag: "logo",
+            child: Image.asset(""))
+
+      ),
+
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/material.dart';
 
+import 'Screens/homeScreen.dart';
+import 'Screens/splash_screen.dart';
+
 void main() {
   runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primaryColor: Colors.deepOrangeAccent
+      ),
+      initialRoute: SplashScreen.id,
+      routes: {
+        SplashScreen.id:(context)=>SplashScreen(),
+        HomeScreen.id:(context)=>HomeScreen(),
+
+      },
+    );
+  }
+}
